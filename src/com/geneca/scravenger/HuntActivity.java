2e3b75783247d0892f46de72c862672c2be9b9ad
danh32 <danh32@gmail.com>
2011-03-22 13:01:59 -0500

    - added comment

--- src/com/geneca/scravenger/HuntActivity.java
+++ src/com/geneca/scravenger/HuntActivity.java
@@ -9,5 +9,6 @@ public class HuntActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //pointless comment
     }
 }
