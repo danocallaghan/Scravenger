17d9c5788901855e64359f45754805c5bdb99f8d
Frank.Messina <Frank.Messina@Geneca.com>
2011-04-06 22:54:57 -0500

    Added proof classes

---
+++ src/com/geneca/scravenger/models/Proof.java
@@ -0,0 +1,35 @@
+package com.geneca.scravenger.models;
+
+public abstract class Proof extends BaseModel{
+    
+    private Leg leg;
+    private boolean isValid;
+    
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+
+	public Leg getLeg() {
+		return leg;
+	}
+	
+	public abstract Proof GetProof();
+
+	public static boolean SubmitProof(Proof proof)
+    {
+    	//Send the proof to the cloud (REST API)
+    	
+    	//
+		return false;
+    	
+    }
+
+	public void setIsValid(boolean isValid) {
+		this.isValid = isValid;
+	}
+
+	public boolean getIsValid() {
+		return isValid;
+	}
+
+}
